affe3e663285301c1c4bbde09852386a5418ede9
Sudaraka Gamage <ashenchamli220@gmail.com>
2022-07-12 12:11:27 +0530

    Media queries for components

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -32,7 +32,7 @@ export default function Intro() {
       <div className="right">
           <div className="wrapper">
             <h2>Hi There, I'm</h2>
-            <h1>Charith Kanchana</h1>
+            <h1>Alex Wassabi</h1>
             <h3>Software <span ref={textRef}></span></h3>
           </div>
           <a href="#portfolio">
